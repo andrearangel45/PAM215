727de5be13eceeb3a9aa40eb2e0ebbd3023c91be
andrearangel45 <124049331@upq.edu.mx>
2025-09-19 20:37:05 -0600

    arrow function
    Cambie la función tradicional por una arrow function que realiza exactamente lo mismo pero se utilizamos unicamente una línea de código.

---
+++ Practica1/EjercicioB.js
@@ -0,0 +1,11 @@
+function cuadrado (numero){
+    return numero * numero;
+}
+
+const cuadrado2 = numero => numero*numero;
+
+console.log(cuadrado2 (2));
+console.log(cuadrado2 (10));
+console.log(cuadrado2 (5));
+
+console.log(cuadrado (2));
---
+++ Practica1/EjercicioC.js
@@ -0,0 +1,4 @@
+
+const saludoPersonal= (nombre, edad) => console.log("Hola, me llamo " + nombre + " y tengo " + edad + " años.");
+
+console.log(saludoPersonal ("Andrea", "20"));
