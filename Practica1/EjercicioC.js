62f25d0b14b75b4e954280e02c021064b46b525a
andrearangel45 <124049331@upq.edu.mx>
2025-09-19 20:39:47 -0600

    SaludoPersonal
    Realice una arrow function que reciba dos parametros nombre y edad y que me retorne un saludo completo.

--- Practica1/EjercicioC.js
+++ Practica1/EjercicioC.js
@@ -2,3 +2,4 @@
 const saludoPersonal= (nombre, edad) => console.log("Hola, me llamo " + nombre + " y tengo " + edad + " años.");
 
 console.log(saludoPersonal ("Andrea", "20"));
+
--- Prueba1/EjercicioA.js
+++
@@ -1,5 +0,0 @@
-let nombre = "Armando";
-let edad = 25;
-
-const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
-console.log(saludo);
